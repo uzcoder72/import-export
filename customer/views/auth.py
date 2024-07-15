58732a36dbc07e579e6d6a3769c83c7af52542ea
Asadbek <hunterkpop72@gmail.com>
2024-07-15 19:24:17 +0500

    fix in auth

--- customer/views/auth.py
+++ customer/views/auth.py
@@ -2,10 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import FormView, View
-from django.contrib.auth.mixins import PermissionRequiredMixin
-
 from customer.forms import LoginForm, RegisterModelForm
-
 
 class LoginPageView(FormView):
     template_name = 'auth/login.html'
@@ -21,19 +18,15 @@ class LoginPageView(FormView):
             return super().form_valid(form)
         return self.form_invalid(form)
 
-
 class LogoutPageView(View):
     def get(self, request, *args, **kwargs):
         logout(request)
         return redirect('customers')
 
-
-class RegisterPageView(PermissionRequiredMixin, FormView):
+class RegisterPageView(FormView):
     template_name = 'auth/register.html'
     form_class = RegisterModelForm
     success_url = reverse_lazy('customers')
-    permission_required = 'customer.add_customer'
-    raise_exception = True
 
     def form_valid(self, form):
         user = form.save(commit=False)
